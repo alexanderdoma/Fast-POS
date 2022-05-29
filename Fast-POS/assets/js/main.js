1f042d17ad532255d40476fd094d6fca6576429c
Alexander <alexanderdoma.2002@gmail.com>
2022-05-29 01:31:46 -0500

    Agregando maqueta web

---
+++ Fast-POS/assets/js/main.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    iniciarApp();
+})
+
+function iniciarApp() {
+    configMenuMobile();
+}
+
+function configMenuMobile() {
+    const menu = document.querySelector('.icon-tabler-menu-2');
+    menu.addEventListener('click', function () {
+        const navegacion = document.querySelector('aside');
+        if (navegacion.classList.contains('mostrar')) {
+            navegacion.classList.remove('mostrar');
+        } else {
+            navegacion.classList.add('mostrar');
+        }
+    })
+}
